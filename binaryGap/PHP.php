3e0bbfb934dc73b00af06a24cde1910f560525dd
Emeka <bluegraonics@gmail.com>
2021-01-21 23:23:51 +0100

    binary gap php

--- binaryGap/PHP.php
+++ binaryGap/PHP.php
@@ -1,9 +1,11 @@
 <?php
 
 
+//My move 1
+//Correctness:100%
+//Task Score:100%
+//Performance: Not assessed
 function solution($N){
-
-
     $N = decbin($N); // convert to base 2
     if(!strpos($N,'0')) return 0;
         
@@ -15,23 +17,21 @@ function solution($N){
     $first = $string_list[0];
     $last = $string_list[$list_length - 1];
 
-   
-    
     if($last == '' or $N[$N_length - 1] == '0') array_pop($string_list); // remove the last element. Mutates array
     if ($first=='' or  $N[0] == '0') array_shift($string_list); // remove the first element. Mutates array
 
     $string_set = array_unique($string_list);
     
-
     foreach($string_set as $item){
         $zeros_count = count(explode('0',$item)) - 1; // occurrences of 0
         $max_zeros = $max_zeros < $zeros_count ? $zeros_count : $max_zeros;
     }
    
-        
     return $max_zeros;
     
     }
     
+
+print(solution(600));
 
 
